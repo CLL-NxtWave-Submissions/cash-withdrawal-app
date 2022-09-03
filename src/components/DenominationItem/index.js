1aeb6f6a8c94067c54d4055bdbc2b93a6b301623
Surya T N <suryatej.nim@yahoo.com>
2022-09-03 07:28:04 +0000

    Implemented UI to render data and added even handlers to elements of DenominationItem component in the corresponding index.js

--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,1 +1,17 @@
-// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationValue, cashWithdrawalEventHandler} = props
+
+  return (
+    <button
+      type="button"
+      className="denomination-item-button"
+      onClick={cashWithdrawalEventHandler}
+    >
+      {denominationValue}
+    </button>
+  )
+}
+
+export default DenominationItem
